8dfdcd8558633c66523352e3a71d21dc02bd92d1
jdev082 <willsjayden2@gmail.com>
2023-01-09 16:08:46 -0600

    script: bundle packages

--- bundle.sh
+++ bundle.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+rm -rf Packages
 mkdir -p Packages
-cp -r */deb/build.deb ./Packages/$RAND.deb
+cp -r */deb/build.deb ./Packages/$RANDOM.deb
 git add -A
 git commit -m "script: bundle packages"
 git push
